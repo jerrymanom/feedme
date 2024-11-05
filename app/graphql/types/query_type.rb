3e927c4698cf002ffbd44f58798857be7baf2fc2
jerrymanom <jerrymanom@gmail.com>
2024-11-05 16:18:49 -0700

    linter in query_type file

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 module Types
+  # QueryType define los puntos de entrada para las consultas en el esquema de GraphQL.
   class QueryType < Types::BaseObject
+    description 'El tipo de raíz para todas las consultas en este esquema'
+
     field :node, Types::NodeType, null: true, description: 'Fetches an object given its ID.' do
       argument :id, ID, required: true, description: 'ID of the object.'
-    end
-
-    def node(id:)
-      context.schema.object_from_id(id, context)
     end
 
     field :nodes, [Types::NodeType, { null: true }], null: true,
@@ -15,44 +14,50 @@ module Types
       argument :ids, [ID], required: true, description: 'IDs of the objects.'
     end
 
-    def nodes(ids:)
-      ids.map { |id| context.schema.object_from_id(id, context) }
-    end
-
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
     field :categories, [CategoryType], null: false, description: 'Returns a list of categories'
-    def categories
-      Category.all
-    end
 
     field :category, CategoryType, null: false, description: 'Return data by one category' do
-      argument :id, ID, required: true
+      argument :id, ID, required: true, description: 'ID único de la categoría a retornar'
+    end
+
+    field :products, [ProductType], null: false, description: 'Returns a list of products'
+
+    field :product, ProductType, null: false, description: 'Return data by a product' do
+      argument :id, ID, required: true, description: 'Id from Product table'
+    end
+
+    field :products_by_category, [ProductType], null: false, description: 'Return a list of products by a category' do
+      argument :category_id, ID, required: true, description: 'Category Id from Category table'
+      argument :first, Integer, required: false, default_value: 10, description: 'Value to set the initial range'
+      argument :skip, Integer, required: false, default_value: 0, description: 'Value to seet the final range'
+    end
+
+    def node(id:)
+      context.schema.object_from_id(id, context)
+    end
+
+    def nodes(ids:)
+      ids.map { |id| context.schema.object_from_id(id, context) }
+    end
+
+    def categories
+      Category.all
     end
 
     def category(id:)
       Category.find(id)
     end
 
-    field :products, [ProductType], null: false, description: 'Returns a list of products'
     def products
       Product.all
-    end
-
-    field :product, ProductType, null: false, description: 'Return data by a product' do
-      argument :id, ID, required: true
     end
 
     def product(id:)
       Product.find(id)
     end
-
-    # field :products_by_category, [ProductType], null:false, description: "Return a list of products by a category" do
-    #   argument :category_id, ID, required: true
-    #   argument :first, Integer, required: false, default_value: 10
-    #   argument :skip, Integer, required: false, default_value: 0
-    # end
 
     # def products_by_category(category_id:, first:, skip:)
     #   total_products = Product.where(category_id: category_id)
